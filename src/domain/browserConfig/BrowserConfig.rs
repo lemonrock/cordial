ad80719b300f142cc856f1b557b7e46e67c46716
Raphael Cohn <raphael.cohn@stormmq.com>
2017-12-01 11:08:27 +0000

    Minor refactoring of browser config

--- src/domain/browserConfig/BrowserConfig.rs
+++ src/domain/browserConfig/BrowserConfig.rs
@@ -39,51 +39,21 @@ impl BrowserConfig
 		{
 			eventWriter.writeWithinElement(Name::local("msapplication"), &namespace, &emptyAttributes, |eventWriter|
 			{
-				eventWriter.writeWithinElement(Name::local("tile"), &namespace, &emptyAttributes, |eventWriter|
+				eventWriter.writeWithinElement(Name::local("tile"), &namespace, &emptyAttributes, |mut eventWriter|
 				{
-					let borrowed = Self::pngUrlData(&self.tile_url, resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language, 70, 70)?;
-					let attributes =
-					[
-						XmlAttribute::new(Name::local("src"), borrowed.url_str()),
-					];
-					eventWriter.writeEmptyElement(&namespace, &attributes, Name::local("square70x70logo"))?;
-					
-					let borrowed = Self::pngUrlData(&self.tile_url, resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language, 150, 150)?;
-					let attributes =
-					[
-						XmlAttribute::new(Name::local("src"), borrowed.url_str()),
-					];
-					eventWriter.writeEmptyElement(&namespace, &attributes, Name::local("square150x150logo"))?;
-					
-					let borrowed = Self::pngUrlData(&self.tile_url, resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language, 310, 150)?;
-					let attributes =
-					[
-						XmlAttribute::new(Name::local("src"), borrowed.url_str()),
-					];
-					eventWriter.writeEmptyElement(&namespace, &attributes, Name::local("wide310x150logo"))?;
-					
-					let borrowed = Self::pngUrlData(&self.tile_url, resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language, 310, 310)?;
-					let attributes =
-					[
-						XmlAttribute::new(Name::local("src"), borrowed.url_str()),
-					];
-					eventWriter.writeEmptyElement(&namespace, &attributes, Name::local("square310x310logo"))?;
-					
-					eventWriter.writeTextElement(&namespace, &emptyAttributes, Name::local("TileColor"), &format!("#{:02X}{:02X}{:02X}", self.tile_color[0], self.tile_color[1], self.tile_color[2]))
+					self.writeTileIconElement(&mut eventWriter, &namespace, resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language, 70, 70)?;
+					self.writeTileIconElement(&mut eventWriter, &namespace, resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language, 150, 150)?;
+					self.writeTileIconElement(&mut eventWriter, &namespace, resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language, 310, 150)?;
+					self.writeTileIconElement(&mut eventWriter, &namespace, resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language, 310, 310)?;
+					self.writeTileColorElement(&mut eventWriter, &namespace)
 				})?;
 				
 				if let Some(ref badge_url) = self.badge_url
 				{
-					eventWriter.writeWithinElement(Name::local("badge"), &namespace, &emptyAttributes, |eventWriter|
+					eventWriter.writeWithinElement(Name::local("badge"), &namespace, &emptyAttributes, |mut eventWriter|
 					{
-						let borrowed = Self::xmlUrlData(badge_url, resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language)?;
-						let attributes =
-						[
-							XmlAttribute::new(Name::local("src"), borrowed.url_str()),
-						];
-						eventWriter.writeEmptyElement(&namespace, &attributes, Name::local("polling-uri"))?;
-						
-						eventWriter.writeTextElement(&namespace, &emptyAttributes, Name::local("frequency"), self.badge_poll_frequency.to_str())
+						Self::writePollElement(&mut eventWriter, &namespace, resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language, "polling-uri", badge_url)?;
+						Self::writeFrequencyElement(&mut eventWriter, &namespace, self.badge_poll_frequency)
 					})?;
 				}
 				
@@ -91,25 +61,9 @@ impl BrowserConfig
 				{
 					eventWriter.writeWithinElement(Name::local("notification"), &namespace, &emptyAttributes, |mut eventWriter|
 					{
-						let mut keepWriting = true;
-						for index in 0..4
-						{
-							if keepWriting
-							{
-								keepWriting = self.notificationUrlData(resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language, &mut eventWriter, &namespace, index)?;
-							}
-							else
-							{
-								if self.notification_urls[index].is_some()
-								{
-									return Err(CordialError::Configuration("Non-contiguous poll URIs".to_owned()));
-								}
-							}
-						}
-						
-						eventWriter.writeTextElement(&namespace, &emptyAttributes, Name::local("frequency"), self.notification_poll_frequency.to_str())?;
-						
-						eventWriter.writeTextElement(&namespace, &emptyAttributes, Name::local("cycle"), self.notification_poll_cycle.to_str())
+						self.writePollNotificationElements(resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language, &mut eventWriter, &namespace)?;
+						Self::writeFrequencyElement(&mut eventWriter, &namespace, self.notification_poll_frequency)?;
+						self.writePollCycleElement(&mut eventWriter, &namespace)
 					})?;
 				}
 				
@@ -122,20 +76,37 @@ impl BrowserConfig
 		
 		Ok(body)
 	}
+	
 	#[inline(always)]
-	fn notificationUrlData(&self, resources: &Resources, fallbackIso639Dash1Alpha2Language: Iso639Dash1Alpha2Language, iso639Dash1Alpha2Language: Option<Iso639Dash1Alpha2Language>, eventWriter: &mut EventWriter<Vec<u8>>, namespace: &Namespace, index: usize) -> Result<bool, CordialError>
+	fn writePollNotificationElements(&self, resources: &Resources, fallbackIso639Dash1Alpha2Language: Iso639Dash1Alpha2Language, iso639Dash1Alpha2Language: Option<Iso639Dash1Alpha2Language>, eventWriter: &mut EventWriter<Vec<u8>>, namespace: &Namespace)  -> Result<(), CordialError>
+	{
+		let mut keepWriting = true;
+		for index in 0..4
+		{
+			if keepWriting
+			{
+				keepWriting = self.writePollNotificationElement(resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language, eventWriter, namespace, index)?;
+			}
+			else
+			{
+				if self.notification_urls[index].is_some()
+				{
+					return Err(CordialError::Configuration("Non-contiguous poll URIs".to_owned()));
+				}
+			}
+		}
+		
+		Ok(())
+	}
+	
+	#[inline(always)]
+	fn writePollNotificationElement(&self, resources: &Resources, fallbackIso639Dash1Alpha2Language: Iso639Dash1Alpha2Language, iso639Dash1Alpha2Language: Option<Iso639Dash1Alpha2Language>, eventWriter: &mut EventWriter<Vec<u8>>, namespace: &Namespace, index: usize) -> Result<bool, CordialError>
 	{
 		match self.notification_urls[index]
 		{
 			None => Ok(false),
 			Some(ref resourceUrl) =>
 			{
-				let borrowed = Self::xmlUrlData(resourceUrl, resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language)?;
-				let attributes =
-				[
-					XmlAttribute::new(Name::local("src"), borrowed.url_str()),
-				];
-				
 				let elementName = match index
 				{
 					0 => "polling-uri",
@@ -146,11 +117,64 @@ impl BrowserConfig
 					_ => unreachable!(),
 				};
 				
-				eventWriter.writeEmptyElement(namespace, &attributes, Name::local(elementName))?;
+				Self::writePollElement(eventWriter, namespace, resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language, elementName, resourceUrl)?;
 				
 				Ok(true)
 			}
 		}
+	}
+	
+	#[inline(always)]
+	fn writePollElement(eventWriter: &mut EventWriter<Vec<u8>>, namespace: &Namespace, resources: &Resources, fallbackIso639Dash1Alpha2Language: Iso639Dash1Alpha2Language, iso639Dash1Alpha2Language: Option<Iso639Dash1Alpha2Language>, elementName: &str, resourceUrl: &ResourceUrl) -> Result<(), CordialError>
+	{
+		let urlData = Self::xmlUrlData(resourceUrl, resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language)?;
+		
+		Self::writeEmptyElementWithSrcAttribute(eventWriter, namespace, &elementName, urlData)
+	}
+	
+	#[inline(always)]
+	fn writeFrequencyElement(eventWriter: &mut EventWriter<Vec<u8>>, namespace: &Namespace, frequency: BrowserConfigPollFrequencyInMinutes) -> Result<(), CordialError>
+	{
+		eventWriter.writeTextElement(&namespace, &[], Name::local("frequency"), frequency.to_str())
+	}
+	
+	#[inline(always)]
+	fn writeTileIconElement(&self, eventWriter: &mut EventWriter<Vec<u8>>, namespace: &Namespace, resources: &Resources, fallbackIso639Dash1Alpha2Language: Iso639Dash1Alpha2Language, iso639Dash1Alpha2Language: Option<Iso639Dash1Alpha2Language>, width: u32, height: u32) -> Result<(), CordialError>
+	{
+		let elementName = if width == height
+		{
+			format!("square{}x{}logo", width, height)
+		}
+		else
+		{
+			format!("wide{}x{}logo", width, height)
+		};
+		
+		let urlData = Self::pngUrlData(&self.tile_url, resources, fallbackIso639Dash1Alpha2Language, iso639Dash1Alpha2Language, width, height)?;
+		
+		Self::writeEmptyElementWithSrcAttribute(eventWriter, namespace, &elementName, urlData)
+	}
+	
+	#[inline(always)]
+	fn writeTileColorElement(&self, eventWriter: &mut EventWriter<Vec<u8>>, namespace: &Namespace) -> Result<(), CordialError>
+	{
+		eventWriter.writeTextElement(&namespace, &[], Name::local("TileColor"), &format!("#{:02X}{:02X}{:02X}", self.tile_color[0], self.tile_color[1], self.tile_color[2]))
+	}
+	
+	#[inline(always)]
+	fn writePollCycleElement(&self, eventWriter: &mut EventWriter<Vec<u8>>, namespace: &Namespace) -> Result<(), CordialError>
+	{
+		eventWriter.writeTextElement(&namespace, &[], Name::local("cycle"), self.notification_poll_cycle.to_str())
+	}
+	
+	#[inline(always)]
+	fn writeEmptyElementWithSrcAttribute(eventWriter: &mut EventWriter<Vec<u8>>, namespace: &Namespace, name: &str, urlData: Rc<UrlData>) -> Result<(), CordialError>
+	{
+		let attributes =
+		[
+			XmlAttribute::new(Name::local("src"), urlData.url_str()),
+		];
+		eventWriter.writeEmptyElement(&namespace, &attributes, Name::local(name))
 	}
 	
 	#[inline(always)]
